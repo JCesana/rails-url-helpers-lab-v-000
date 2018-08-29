2182a8321824a4e07746a2c63df12ec8018a9164
Josh Cesana <joshuacesana@gmail.com>
2018-08-29 07:54:24 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
   get "students/:id", to: "students#show", as: "student"
   
   get "students/:id/activate", to: "students#activate", as: "activate_student"
-  
 end
